e3de360be263a5deb9fac748e03f1071c35510e1
MahadiPersonal <Mahadi4@gmail.com>
2019-01-23 11:49:12 +0100

    Implemented the minimax AI

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var origBoard;
-const huPlayer = '0';
+const huPlayer = 'O';
 const aiPlayer = 'X';
 const winCombos = [
 	[0, 1, 2],
@@ -15,7 +15,7 @@ const winCombos = [
 const cells = document.querySelectorAll('.cell');
 startGame();
 
-function startGame () {
+function startGame() {
 	document.querySelector(".endgame").style.display = "none";
 	origBoard = Array.from(Array(9).keys());
 	for (var i = 0; i < cells.length; i++) {
@@ -27,21 +27,20 @@ function startGame () {
 
 function turnClick(square) {
 	if (typeof origBoard[square.target.id] == 'number') {
-		turn(square.target.id, huPlayer);
-		if (!checkTie()) turn(bestSpot(), aiPlayer);
+		turn(square.target.id, huPlayer)
+		if (!checkWin(origBoard, huPlayer) && !checkTie()) turn(bestSpot(), aiPlayer);
 	}
-	//console.log(square.target.id);
 }
 
 function turn(squareId, player) {
 	origBoard[squareId] = player;
 	document.getElementById(squareId).innerText = player;
-	let gameWon = checkWin(origBoard, player);
-	if(gameWon) gameOver(gameWon)
+	let gameWon = checkWin(origBoard, player)
+	if (gameWon) gameOver(gameWon)
 }
 
 function checkWin(board, player) {
-	let plays = board.reduce((a, e, i) => 
+	let plays = board.reduce((a, e, i) =>
 		(e === player) ? a.concat(i) : a, []);
 	let gameWon = null;
 	for (let [index, win] of winCombos.entries()) {
@@ -61,7 +60,7 @@ function gameOver(gameWon) {
 	for (var i = 0; i < cells.length; i++) {
 		cells[i].removeEventListener('click', turnClick, false);
 	}
-	declareWinner(gameWon.player == huPlayer ? "You Win!" : "You lose!");
+	declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose.");
 }
 
 function declareWinner(who) {
@@ -74,17 +73,71 @@ function emptySquares() {
 }
 
 function bestSpot() {
-	return emptySquares()[0];
+	return minimax(origBoard, aiPlayer).index;
 }
 
 function checkTie() {
-	if (emptySquares().length === 0) {
+	if (emptySquares().length == 0) {
 		for (var i = 0; i < cells.length; i++) {
 			cells[i].style.backgroundColor = "green";
 			cells[i].removeEventListener('click', turnClick, false);
 		}
-		declareWinner("Tie Game!");
+		declareWinner("Tie Game!")
 		return true;
 	}
 	return false;
+}
+
+/*
+	https://medium.freecodecamp.org/how-to-make-your-tic-tac-toe-game-unbeatable-by-using-the-minimax-algorithm-9d690bad4b37
+*/
+function minimax(newBoard, player) {
+	var availSpots = emptySquares();
+
+	if (checkWin(newBoard, huPlayer)) {
+		return {score: -10};
+	} else if (checkWin(newBoard, aiPlayer)) {
+		return {score: 10};
+	} else if (availSpots.length === 0) {
+		return {score: 0};
+	}
+	var moves = [];
+	for (var i = 0; i < availSpots.length; i++) {
+		var move = {};
+		move.index = newBoard[availSpots[i]];
+		newBoard[availSpots[i]] = player;
+
+		if (player == aiPlayer) {
+			var result = minimax(newBoard, huPlayer);
+			move.score = result.score;
+		} else {
+			var result = minimax(newBoard, aiPlayer);
+			move.score = result.score;
+		}
+
+		newBoard[availSpots[i]] = move.index;
+
+		moves.push(move);
+	}
+
+	var bestMove;
+	if(player === aiPlayer) {
+		var bestScore = -10000;
+		for(var i = 0; i < moves.length; i++) {
+			if (moves[i].score > bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	} else {
+		var bestScore = 10000;
+		for(var i = 0; i < moves.length; i++) {
+			if (moves[i].score < bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	}
+
+	return moves[bestMove];
 }
